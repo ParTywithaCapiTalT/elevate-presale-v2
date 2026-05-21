45b44e2c9e73ec42133cdf30c2e3c514c99f7a81
ParTywithaCapiTalT <javi.sant.castro@gmail.com>
2026-05-21 06:56:43 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),     // ← This was missing
+    tailwindcss(),
   ],
   base: '/',
 })
